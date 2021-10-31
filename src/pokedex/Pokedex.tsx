c73791987f5abe3dd0c33ecf6d17c47f798b8090
Jean Izepon <izeponjc@gmail.com>
2021-10-31 10:44:27 -0300

    create: consulta PokeAPI com Axios

--- src/pokedex/Pokedex.tsx
+++ src/pokedex/Pokedex.tsx
@@ -1,25 +1,54 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 interface PokedexProps {}
 
-const pokemonsArray: string[] = ["Pikachu", "Scizor", "Umbreon"];
+interface PokemonListInterface {
+  name: string;
+  url: string;
+}
+
+// async function getDetailsFromPokemon(pokemon: PokemonListInterface) {
+//     await
+// }
 
 export const Pokedex: React.FC<PokedexProps> = () => {
-  const [pokemons, setPokemons] = useState<string[]>(pokemonsArray);
-  const [selectedPokemon, setSelectedPokemon] = useState<string | undefined>(
-    undefined
-  );
+  const [pokemons, setPokemons] = useState<PokemonListInterface[]>([]);
+  const [selectedPokemon, setSelectedPokemon] = useState<
+    PokemonListInterface | undefined
+  >(undefined);
+  const [selectedPokemonDetails, setSelectedPokemonDetails] = useState<
+    any | undefined
+  >(undefined);
+
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon")
+      .then((response) => setPokemons(response.data.results));
+  }, []);
+
+  useEffect(() => {
+    if (!selectedPokemon) return;
+
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${selectedPokemon.name}`)
+      .then((response) => setSelectedPokemonDetails(response.data));
+  }, [selectedPokemon]);
 
   return (
     <div>
       <h1>Pokedex</h1>
       Pokemons:
       {pokemons.map((pokemon) => (
-        <button onClick={() => setSelectedPokemon(pokemon)}>{pokemon}</button>
+        <button onClick={() => setSelectedPokemon(pokemon)}>
+          {pokemon.name}
+        </button>
       ))}
       <h2>
-        Pokemon selecionado: {selectedPokemon || "Nenhum Pokémon selecionado."}
+        Pokemon selecionado:{" "}
+        {selectedPokemon?.name || "Nenhum Pokémon selecionado."}
       </h2>
+      {JSON.stringify(selectedPokemonDetails, undefined, 2)}
     </div>
   );
 };
