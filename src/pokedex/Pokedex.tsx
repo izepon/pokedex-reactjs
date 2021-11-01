040e015bdb7b716b8deb9bb1ad1f45223fbddef9
Jean Izepon <izeponjc@gmail.com>
2021-11-01 13:29:11 -0300

    create: estilo inicial com material ui

--- src/pokedex/Pokedex.tsx
+++ src/pokedex/Pokedex.tsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { getPokemonDetails } from "../pokemon/services/getPokemonDetails";
 import { PokemonDetail } from "../pokemon/interfaces/PokemonDetail";
@@ -6,6 +5,17 @@ import {
   listPokemon,
   PokemonListInterface,
 } from "../pokemon/services/listPokemons";
+
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
 
 interface PokedexProps {}
 
@@ -32,18 +42,46 @@ export const Pokedex: React.FC<PokedexProps> = () => {
 
   return (
     <div>
-      <h1>Pokedex</h1>
-      Pokemons:
-      {pokemons.map((pokemon) => (
-        <button onClick={() => setSelectedPokemon(pokemon)}>
-          {pokemon.name}
-        </button>
-      ))}
-      <h2>
-        Pokemon selecionado:{" "}
-        {selectedPokemon?.name || "Nenhum Pokémon selecionado."}
-      </h2>
-      {JSON.stringify(selectedPokemonDetails, undefined, 2)}
+      {/* AppBar */}
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h4">Pokedex</Typography>
+        </Toolbar>
+      </AppBar>
+      {/* Fim AppBar */}
+      <Container maxWidth="lg">
+        <Box mt={2}>
+          <Grid container spacing={2}>
+            {pokemons.map((pokemon) => (
+              <>
+                <Grid item xs={6} lg={3}>
+                  <Card>
+                    <CardContent>
+                      <Typography variant="h5" component="h2">
+                        {pokemon.name}
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button
+                        onClick={() => setSelectedPokemon(pokemon)}
+                        size="small"
+                      >
+                        Abrir
+                      </Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              </>
+            ))}
+          </Grid>
+          Pokemons:
+          <h2>
+            Pokemon selecionado:{" "}
+            {selectedPokemon?.name || "Nenhum Pokémon selecionado."}
+          </h2>
+          {JSON.stringify(selectedPokemonDetails, undefined, 2)}
+        </Box>
+      </Container>
     </div>
   );
 };
